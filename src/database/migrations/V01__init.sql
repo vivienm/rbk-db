36eeb05ead4e8b1c4ea476aa3b10713752a48ff0
Vivien Maisonneuve <v.maisonneuve@gmail.com>
2026-04-12 16:58:22 +0200

    fix: Add CHECK constraint on rel_type in part_relationships
    For consistency with part_material in the parts table. Also reformat
    the SQL schema for readability.

--- src/database/migrations/V01__init.sql
+++ src/database/migrations/V01__init.sql
@@ -1,8 +1,10 @@
 CREATE TABLE colors (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
-    rgb TEXT NOT NULL CHECK (rgb GLOB '[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]'),
-    is_trans INTEGER NOT NULL CHECK (is_trans IN (0, 1))
+    rgb TEXT NOT NULL
+        CHECK (rgb GLOB '[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]'),
+    is_trans INTEGER NOT NULL
+        CHECK (is_trans IN (0, 1))
 ) STRICT;
 
 CREATE TABLE part_categories (
@@ -11,47 +13,66 @@ CREATE TABLE part_categories (
 ) STRICT;
 
 CREATE TABLE parts (
-    part_num TEXT PRIMARY KEY CHECK (part_num NOT GLOB 'fig-[0-9][0-9][0-9][0-9][0-9][0-9]'),
+    part_num TEXT PRIMARY KEY
+        CHECK (part_num NOT GLOB 'fig-[0-9][0-9][0-9][0-9][0-9][0-9]'),
     name TEXT NOT NULL,
-    part_cat_id INTEGER NOT NULL REFERENCES part_categories(id),
-    part_material TEXT NOT NULL CHECK (part_material IN ('cardboard/paper', 'cloth', 'flexible plastic', 'foam', 'metal', 'plastic', 'rubber'))
+    part_cat_id INTEGER NOT NULL
+        REFERENCES part_categories(id),
+    part_material TEXT NOT NULL
+        CHECK (part_material IN (
+            'cardboard/paper', 'cloth', 'flexible plastic', 'foam',
+            'metal', 'plastic', 'rubber'
+        ))
 ) STRICT;
 
 -- NOTE: There might be several relationships between the same parts, with
 -- different types.
 CREATE TABLE part_relationships (
-    rel_type TEXT NOT NULL,
-    child_part_num TEXT NOT NULL REFERENCES parts(part_num),
-    parent_part_num TEXT NOT NULL REFERENCES parts(part_num),
+    rel_type TEXT NOT NULL
+        CHECK (rel_type IN (
+            'print', 'pair', 'subpart', 'mold', 'pattern', 'alternate'
+        )),
+    child_part_num TEXT NOT NULL
+        REFERENCES parts(part_num),
+    parent_part_num TEXT NOT NULL
+        REFERENCES parts(part_num),
     UNIQUE (rel_type, child_part_num, parent_part_num)
 ) STRICT;
 
 CREATE TABLE elements (
     element_id TEXT PRIMARY KEY,
-    part_num TEXT NOT NULL REFERENCES parts(part_num),
-    color_id INTEGER NOT NULL REFERENCES colors(id),
+    part_num TEXT NOT NULL
+        REFERENCES parts(part_num),
+    color_id INTEGER NOT NULL
+        REFERENCES colors(id),
     design_id INTEGER
 ) STRICT;
 
 CREATE TABLE minifigs (
-    fig_num TEXT PRIMARY KEY CHECK (fig_num GLOB 'fig-[0-9][0-9][0-9][0-9][0-9][0-9]'),
+    fig_num TEXT PRIMARY KEY
+        CHECK (fig_num GLOB 'fig-[0-9][0-9][0-9][0-9][0-9][0-9]'),
     name TEXT NOT NULL,
-    num_parts INTEGER NOT NULL CHECK (num_parts >= 0),
+    num_parts INTEGER NOT NULL
+        CHECK (num_parts >= 0),
     img_url TEXT NOT NULL
 ) STRICT;
 
 CREATE TABLE themes (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
-    parent_id INTEGER REFERENCES themes(id)
+    parent_id INTEGER
+        REFERENCES themes(id)
 ) STRICT;
 
 CREATE TABLE sets (
     set_num TEXT PRIMARY KEY,
     name TEXT NOT NULL,
-    year INTEGER NOT NULL CHECK (year >= 1932),
-    theme_id INTEGER NOT NULL REFERENCES themes(id),
-    num_parts INTEGER NOT NULL CHECK (num_parts >= 0),
+    year INTEGER NOT NULL
+        CHECK (year >= 1932),
+    theme_id INTEGER NOT NULL
+        REFERENCES themes(id),
+    num_parts INTEGER NOT NULL
+        CHECK (num_parts >= 0),
     img_url TEXT NOT NULL
 ) STRICT;
 
@@ -64,11 +85,16 @@ CREATE TABLE inventories (
 ) STRICT;
 
 CREATE TABLE inventory_parts (
-    inventory_id INTEGER NOT NULL REFERENCES inventories(id),
-    part_num TEXT NOT NULL REFERENCES parts(part_num),
-    color_id INTEGER NOT NULL REFERENCES colors(id),
-    quantity INTEGER NOT NULL CHECK (quantity >= 1),
-    is_spare INTEGER NOT NULL CHECK (is_spare IN (0, 1)),
+    inventory_id INTEGER NOT NULL
+        REFERENCES inventories(id),
+    part_num TEXT NOT NULL
+        REFERENCES parts(part_num),
+    color_id INTEGER NOT NULL
+        REFERENCES colors(id),
+    quantity INTEGER NOT NULL
+        CHECK (quantity >= 1),
+    is_spare INTEGER NOT NULL
+        CHECK (is_spare IN (0, 1)),
     img_url TEXT,
     UNIQUE (inventory_id, part_num, color_id, is_spare)
 ) STRICT;
@@ -76,15 +102,21 @@ CREATE TABLE inventory_parts (
 CREATE INDEX inventory_parts_part_num_idx ON inventory_parts(part_num);
 
 CREATE TABLE inventory_minifigs (
-    inventory_id INTEGER NOT NULL REFERENCES inventories(id),
-    fig_num TEXT NOT NULL REFERENCES minifigs(fig_num),
-    quantity INTEGER NOT NULL CHECK (quantity >= 1),
+    inventory_id INTEGER NOT NULL
+        REFERENCES inventories(id),
+    fig_num TEXT NOT NULL
+        REFERENCES minifigs(fig_num),
+    quantity INTEGER NOT NULL
+        CHECK (quantity >= 1),
     UNIQUE (inventory_id, fig_num)
 ) STRICT;
 
 CREATE TABLE inventory_sets (
-    inventory_id INTEGER NOT NULL REFERENCES inventories(id),
-    set_num TEXT NOT NULL REFERENCES sets(set_num),
-    quantity INTEGER NOT NULL CHECK (quantity >= 1),
+    inventory_id INTEGER NOT NULL
+        REFERENCES inventories(id),
+    set_num TEXT NOT NULL
+        REFERENCES sets(set_num),
+    quantity INTEGER NOT NULL
+        CHECK (quantity >= 1),
     UNIQUE (inventory_id, set_num)
 ) STRICT;
